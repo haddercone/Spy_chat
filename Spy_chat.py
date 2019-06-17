ae990249ae8cb5d229af5df31d832c12d8c6996b
Robin Singh <32171406+haddercone@users.noreply.github.com>
2019-06-17 15:41:46 +0530

    Update Spy_chat.py
    updated

--- Spy_chat.py
+++ Spy_chat.py
@@ -1,6 +1,4 @@
-# Create function for adding new spy and to show default spy and use age validation
-
-all_spy_details = [{'name': 'James', 'age': 29, 'experience': '3 years', 'ratings': 3}]
+all_spy_details = [{'name': 'James', 'age': 29, 'experience': '3 years', 'status': 'intermediate', 'rating(s)': 3}]
 
 
 def show_default_spy():
@@ -11,18 +9,25 @@ def add_new_spy():
     while True:
         details = {}
         while True:
-            name = input("Enter name: ")
-            if name.isalpha() is False:
-                print("Name should be a string")
-                continue
-            else:
-                if len(name) >= 3:
-                    details['name'] = name
-                    break
-                else:
-                    print("Name of your spy should be minimum 3 characters long")
-                    print("Please enter the spy name again.")
+            salutation = input("Enter Salutation: ")
+            salutation = salutation.capitalize()
+            if salutation == 'Mr' or salutation == 'Mrs' or salutation == 'Ms':
+                name = input("Enter name: ")
+                name = name.capitalize()
+                if name.isalpha() is False:
+                    print("Name should be a string")
                     continue
+                else:
+                    if len(name) >= 3:
+                        details['name'] = salutation + ". " + name
+                        break
+                    else:
+                        print("Name of your spy should be minimum 3 characters long")
+                        print("Please enter the spy name again.")
+                        continue
+            else:
+                print("Salutation should start from Mr., Mrs., Ms.")
+                continue
         while True:
             age = input("Enter age of your spy: ")
             if age.isdigit() is False:
@@ -42,30 +47,48 @@ def add_new_spy():
                 print("Experience should be in digits: ")
                 continue
             else:
-                experience = str(experience) + " years"
-                details['experience'] = experience
+                if int(experience) >=1 and int(experience) <=5:
+                    experience = str(experience) + " years"
+                    details['experience'] = experience
+                    break
+
+        while True:
+            status = input("Enter the status of the spy: ")
+            if status.isalpha() is False:
+                print("Your status should be a string")
+                continue
+            else:
+                details['status'] = status
                 break
 
         while True:
-            spy_ratings = int(input("Enter the spy ratings: "))
-            if spy_ratings >=1 and spy_ratings <= 5:
-                details['ratings'] = spy_ratings
-                all_spy_details.append(details)
-                break
+            spy_ratings = input("Enter the spy ratings: ")
+            if spy_ratings.isdigit() is False:
+                print("Spy rating should be in digits: ")
             else:
-                print("Rating must be in the range of 1 to 5")
-                continue
+                if int(spy_ratings) >=1 and int(spy_ratings) <= 5:
+                    details['rating(s)'] = int(spy_ratings)
+                    print("Welcome on board ", details['name'])
+                    print("Here are your details: ")
+                    print("\nYour Name: ", details['name'])
+                    print("Your Age: ", details['age'])
+                    print("Your Experience: ", details['experience'])
+                    print("Your Status: ", details['status'])
+                    print("Your Rating: ", details['rating(s)'])
 
+                    all_spy_details.append(details)
+                    break
+                else:
+                    print("Rating must be in the range of 1 to 5")
+                    continue
 
-        confirmation = input('Want to add more spies [y/n]?: ')
+        confirmation = input('\nWant to add more spies [y/n]?: ')
         confirmation = confirmation.lower()
 
         if confirmation == 'y':
             continue
-        elif confirmation == 'n':
-            break
         else:
-            print("Please choose between 'y' and 'n'")
+            print("Please enter 'y' to add details")
             break
 
 
@@ -89,18 +112,15 @@ def choice():
         else:
             print("Please choose among the above specified keys")
 
-        choice_value = input("Want to execute any specified function again [y/n]? : ")
+        choice_value = input("Want to execute any specified function again [y]? : ")
         choice_value = choice_value.lower()
 
         if choice_value == 'y':
             continue
-        elif choice_value == 'n':
-            break
         else:
-            print("Please choose between 'y' and 'n' ")
+            print("Please enter 'y' to execute the functions ")
             break
 
 
-choice()
-
-
+if __name__ == '__main__':
+    choice()
